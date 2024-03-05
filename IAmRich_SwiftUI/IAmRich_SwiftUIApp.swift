ee1e3f55bc1218f8a707b3f494a0600ffe2e1cab
VladVorniceanu <vorniceanu.v@gmail.com>
2024-03-05 10:32:34 +0200

    Initial Commit

---
+++ IAmRich_SwiftUI/IAmRich_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRich_SwiftUIApp.swift
+//  IAmRich_SwiftUI
+//
+//  Created by Vlad Vorniceanu on 05.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct IAmRich_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
